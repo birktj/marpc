9bff2e2f25140bd78a9959eb46a59ac8234b1509
Birk Tjelmeland <git@birktj.no>
2023-07-29 03:09:21 +0200

    Make example more substansial

--- examples/add.rs
+++ examples/add.rs
@@ -1,5 +1,14 @@
 use std::future::Future;
 use std::pin::Pin;
+use std::sync::{mpsc, OnceLock};
+
+struct Message {
+    uri: &'static str,
+    payload: Vec<u8>,
+    response_channel: mpsc::Sender<Vec<u8>>,
+}
+
+static QUEUE: OnceLock<mpsc::SyncSender<Message>> = OnceLock::new();
 
 struct Service;
 
@@ -16,7 +25,18 @@ impl marpc::ClientRpcService for Service {
         payload: &'a [u8],
     ) -> Pin<Box<dyn 'a + Future<Output = Result<Vec<u8>, Self::ClientError>>>> {
         Box::pin(async move {
-            Ok(marpc::handle_rpc::<Service>(uri, (), payload).await?)
+            let (send, recv) = mpsc::channel();
+
+            let message = Message {
+                uri,
+                payload: payload.to_owned(),
+                response_channel: send,
+            };
+            // Send the rpc call to the "server"
+            QUEUE.get().unwrap().send(message).unwrap();
+
+            // Wait for a response
+            Ok(recv.recv().unwrap())
         })
     }
 }
@@ -33,8 +53,20 @@ async fn test(a: i32, b: i32) -> Result<i32, ()> {
 }
 
 fn main() {
+    let (send, recv) = mpsc::sync_channel(1);
+    QUEUE.set(send).unwrap();
+
+    std::thread::spawn(move || {
+        pollster::block_on(async move {
+            while let Ok(message) = recv.recv() {
+                let res = marpc::handle_rpc::<Service>(message.uri, (), &message.payload).await.unwrap();
+                message.response_channel.send(res).unwrap();
+            }
+        });
+    });
+
     pollster::block_on(async {
         let res = test(5, 6).await;
-        eprintln!("Res: {:?}", res);
-    })
+        println!("Res: {:?}", res);
+    });
 }
